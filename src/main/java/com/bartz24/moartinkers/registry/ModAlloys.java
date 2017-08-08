2c63bc3788b3ce8d783dd039772e2a40134f081d
Bartz24 <bartz24games@gmail.com>
2017-08-08 11:27:37 -0400

    0.5.2

--- src/main/java/com/bartz24/moartinkers/registry/ModAlloys.java
+++ src/main/java/com/bartz24/moartinkers/registry/ModAlloys.java
@@ -3,12 +3,15 @@ package com.bartz24.moartinkers.registry;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.oredict.OreDictionary;
+import slimeknights.mantle.util.RecipeMatch;
 import slimeknights.tconstruct.library.TinkerRegistry;
-import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+import slimeknights.tconstruct.library.smeltery.MeltingRecipe;
 
 public class ModAlloys {
 	public static void init() {
@@ -19,9 +22,26 @@ public class ModAlloys {
 			registerAlloy("constantan*2", "copper*1", "nickel*1");
 		registerAlloy("enderium*144", "lead*108", "platinum*36", "ender*250");
 		registerAlloy("electrumflux*144", "electrum*144", "redstone*500");
-		if(Loader.isModLoaded("thermalfoundation"))
+		if (Loader.isModLoaded("thermalfoundation")) {
 			TinkerRegistry.registerSmelteryFuel(new FluidStack(FluidRegistry.getFluid("pyrotheum"), 50), 400);
-		
+
+			TinkerRegistry
+					.registerMelting(new MeltingRecipe(new RecipeMatch.Item(new ItemStack(Items.REDSTONE), 1, 100),
+							FluidRegistry.getFluid("redstone"), 800));
+			TinkerRegistry.registerMelting(
+					new MeltingRecipe(new RecipeMatch.Item(new ItemStack(Blocks.REDSTONE_BLOCK), 1, 900),
+							FluidRegistry.getFluid("redstone"), 1400));
+			TinkerRegistry.registerMelting(
+					new MeltingRecipe(new RecipeMatch.Item(new ItemStack(Items.GLOWSTONE_DUST), 1, 250),
+							FluidRegistry.getFluid("glowstone"), 1100));
+			TinkerRegistry
+					.registerMelting(new MeltingRecipe(new RecipeMatch.Item(new ItemStack(Blocks.GLOWSTONE), 1, 1000),
+							FluidRegistry.getFluid("glowstone"), 1500));
+			TinkerRegistry
+					.registerMelting(new MeltingRecipe(new RecipeMatch.Item(new ItemStack(Items.ENDER_PEARL), 1, 250),
+							FluidRegistry.getFluid("ender"), 1200));
+		}
+
 	}
 
 	public static void registerAlloy(String out, String... in) {
